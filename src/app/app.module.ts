afd9a50be11d819f050727b035ce9d238c7e45ef
Anuroop L. Akula <anuroop_sai@live.com>
2018-06-26 23:32:21 +0530

    TEST - Configure NGPRIME - Anuroop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { InputTextModule, ButtonModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
